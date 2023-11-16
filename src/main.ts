d8ebdc877d25777888f8eee62428c8a0c703d645
raisyaf20 <raisyafmuhamad@gmail.com>
2023-11-16 16:01:36 +0700

    fix add husskiy semicolon and singlequuote

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import express, { Application, Request, Response } from "express";
+import express, { type Application, type Request, type Response } from 'express'
 
-const app: Application = express();
-const port: number = 8000;
+const app: Application = express()
+const port: number = 8000
 
-app.use("/", (req: Request, res: Response) => {
-  res.status(200).send({ data: "FAIYASHA" });
-});
+app.use('/', (req: Request, res: Response) => {
+  res.status(200).send({ data: 'FAIYASHA' })
+})
 
-app.listen(port, () => console.info(`listen in port http://localhost:${port}`));
+app.listen(port, () => {
+  console.info(`listen in port http://localhost:${port}`)
+})
